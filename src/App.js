92cc58327963d9793b4b99b573f6f7552147450a
twangyal <tsering.w1029@gmail.com>
2024-02-07 21:34:59 -0800

    start

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <header>
+      <HomeButton/>
       <SearchBar />
     </header>
     
   );
 }
 function HomeButton(){
-
+  return(
+    <button>Home</button>
+  );
 }
 function SearchBar(){
   return(
