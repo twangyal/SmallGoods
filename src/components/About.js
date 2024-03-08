3b5763870d7574828a5f9ad1e64f4d4b0aeb16cb
Diego Arevalo Fernandez <darevalofernandez@ucsd.edu>
2024-03-08 01:52:04 -0800

    Try it

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,56 @@
 export default function About(){
     return(
+
         <div>
-            <h1>Smallgoods specializes in small-batch, seasonal, and regional American farmstead cheeses & cured meats. 
-                Smallgoods is San Diego and La Jolla's source for the best American cheeses, 
-                charcuterie, gourmet sandwiches, coffee, deli and Farmer Market offerings. 
-                They currently need a complete overhaul of their website: </h1>
-        </div>
+      <p>
+        Smallgoods specializes in small-batch, seasonal, and regional American
+        farmstead cheeses & cured meats. Smallgoods is San Diego and La Jolla's
+        source for the best American cheeses, charcuterie, gourmet sandwiches,
+        coffee, deli, and Farmer Market offerings.
+      </p>
+      <p>
+        They currently need a complete overhaul of their website. Check out
+        their{' '}
+        <a href="http://www.smallgoodsusa.com/" target="_blank" rel="noopener noreferrer">
+          current website
+        </a>{' '}
+        and their{' '}
+        <a
+          href="https://www.instagram.com/smallgoods_/?hl=en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Instagram
+        </a>
+        .
+      </p>
+      <p>
+        Read the{' '}
+        <a
+          href="https://docs.google.com/document/d/1baUIdLmcVphQEmrQe9jI6vfcqYmk8_L0obL5CNQB8RU/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          project proposal
+        </a>{' '}
+        to learn more.
+      </p>
+      <p>
+        Access the{' '}
+        <a
+          href="https://www.dropbox.com/scl/fo/ctrnmuc131ail462l12mg/h?dl=0&rlkey=rkh6shtbtoodd5q9bl32a0om1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Assets Drop Box
+        </a>{' '}
+        for project assets.
+      </p>
+    </div>
+
         
         
+
         
     )
 }
