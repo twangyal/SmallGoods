aed570aaf85ed140cb2d74c92cc2434e8e952a58
Diego Arevalo Fernandez <darevalofernandez@ucsd.edu>
2024-03-08 01:35:21 -0800

    First edit

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,13 @@
 export default function About(){
     return(
-        <h1>Hello, this is the about page. It's about us!</h1>
+        <div>
+            <h1>Smallgoods specializes in small-batch, seasonal, and regional American farmstead cheeses & cured meats. 
+                Smallgoods is San Diego and La Jolla's source for the best American cheeses, 
+                charcuterie, gourmet sandwiches, coffee, deli and Farmer Market offerings. 
+                They currently need a complete overhaul of their website: </h1>
+        </div>
+        
+        
+        
     )
 }
